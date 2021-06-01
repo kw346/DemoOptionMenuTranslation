b3230bc2f97d20b777aecbbf858ea128f51a77b1
kw346 <20022735@myrp.edu.sg>
2021-06-01 13:10:30 +0800

    done(1)

--- app/src/main/java/sg/edu/rp/c346/id20022735/demooptionmenutranslation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022735/demooptionmenutranslation/MainActivity.java
@@ -3,13 +3,17 @@ package sg.edu.rp.c346.id20022735.demooptionmenutranslation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvTranslatedText;
+    TextView tvTranslatedText2;
+    String wc = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,30 +21,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvTranslatedText = findViewById(R.id.textViewTT);
+        tvTranslatedText2 = findViewById(R.id.textView2);
         registerForContextMenu(tvTranslatedText);
+        registerForContextMenu(tvTranslatedText2);
     }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0, 0, 0, "English");
+        menu.add(0, 1, 1, "Italian");
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.EnglishSelection) {
-            tvTranslatedText.setText("Hello ");
-            return true;
-        } else if (id == R.id.italianSelection) {
-            tvTranslatedText.setText("Ciao");
-            return true;
-        } else {
-            tvTranslatedText.setText("Error translation");
-        }
-
+            if (id == R.id.EnglishSelection) {
+                tvTranslatedText.setText("Hello ");
+                tvTranslatedText2.setText("Bye ");
+                return true;
+            } else if (id == R.id.italianSelection) {
+                tvTranslatedText.setText("Ciao");
+                tvTranslatedText2.setText("Addio");
+                return true;
+            } else {
+                tvTranslatedText.setText("Error translation");
+            }
         return super.onOptionsItemSelected(item);
-    }
+
+            }
+
 
 }
